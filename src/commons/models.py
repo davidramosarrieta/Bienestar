c46008b79e4ed5fcce2e5f79cfc43ac6204ffe8f
Edwin Mendez <emendez@gravitalsoftware.com>
2017-08-15 09:26:11 -0500

    ejemplo_modelo

--- src/commons/models.py
+++ src/commons/models.py
@@ -1,1 +1,21 @@
 # -*- coding: utf-8 -*-
+from src.api import models
+
+
+#class OrganizationLevelInstance(models.Model):
+#
+#    organization_level = models.ForeignKey(verbose_name=_('Ni'))
+#    name = models.CharField(_('Nombre'), max_length=255)
+#    external_website = models.CharField(_('Sitio web externo'), max_length=255, blank=True, null=True)
+#    abbreviation = models.CharField(_('Abreviación'), max_length=255, blank=True, null=True)
+#    country = models.ForeignKey(Country, verbose_name=_('País'), blank=True, null=True)
+#    state = models.ForeignKey(State, verbose_name=_('Departamento'), blank=True, null=True)
+#    city = models.ForeignKey(City, verbose_name=_('Ciudad administrativa'), blank=True, null=True)
+#    address = models.CharField(_('Dirección'), max_length=512, blank=True, null=True)
+#    lat = models.CharField(_('Latitud'), max_length=512, blank=True, null=True)
+#    lng = models.CharField(_('Longitud'), max_length=512, blank=True, null=True)
+#    picture = models.ImageField(_('Imagen'), upload_to=get_instance_path, max_length=512, blank=True, null=True)
+#    phone = models.CharField(_('Teléfono'), max_length=512, default="")
+#    parent = models.ForeignKey('self', blank=True, null=True,verbose_name=_('Sede de la cual depende'))
+#    city_fractions = models.ManyToManyField(CityFraction, verbose_name=_('Ciudades a cargo'), related_name='cities')
+#    place_id = models.CharField(_('Id de sitio en google maps'), max_length=255, blank=True, null=True)
