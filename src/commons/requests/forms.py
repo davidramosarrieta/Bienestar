7c4d7cbdc800bbee64d851b78cafa9d831e9bc57
Edwin Mendez <emendez@gravitalsoftware.com>
2017-08-15 18:33:05 -0500

    formulario de peticion

--- src/commons/requests/forms.py
+++ src/commons/requests/forms.py
@@ -1,19 +1,30 @@
 # -*- coding: utf-8 -*-
-from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django import forms
 from django.contrib.auth.models import Group
 from django.utils.translation import ugettext_lazy as _
 
-from src.commons.models import Need, StudentRequest
+from src.commons.models import Need, StudentRequest, Schedule
 
 
 class RequestForm(forms.Form):
-    description = forms.CharField(widget=CKEditorUploadingWidget(), required=False)
-    need = forms.ModelChoiceField(label=_('Tipo de necesidad'), queryset=Need.objects.none(),
+    description= forms.CharField(label=_('Descripcion'),
+                           widget=forms.Textarea(attrs={
+                               'required':'',
+                                'class':'form-control gui-input',
+                               'placeholder':'Has un comentario de tu solicitud.'
+                           }), required=False)
+    need = forms.ModelChoiceField(label=_('Tipo de necesidad'), queryset=Need.objects.all(),
+                            widget=forms.Select(attrs={
+                                'required': '',
+                                'class':'form-control gui-input',
+                                'placeholder':'solicitud.'
+                            }))
+    schedule = forms.ModelChoiceField(label=_('Tipo de necesidad'), queryset=Schedule.objects.all(),
                             widget=forms.Select(attrs={
                                 'required': '',
                                 'class':'form-control gui-input'
                             }))
+
 
     def __init__(self, request, *args, **kwargs):
         super(RequestForm, self).__init__(*args, **kwargs)
@@ -24,13 +35,9 @@ class RequestForm(forms.Form):
             instance = StudentRequest(
             description = self.cleaned_data['description'],
             need = self.cleaned_data['need'],
+            schedule=self.cleaned_data['schedule'],
             student = self.student,
-
             )
-            instance.save()
-        else:
-            instance.description=self.cleaned_data['description']
-            instance.need=self.cleaned_data['need']
             instance.save()
 
         return instance
