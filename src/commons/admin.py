01db1b2dd7efd5f683d2bd6472d33ce3f2413785
Edwin Mendez <emendez@gravitalsoftware.com>
2017-08-15 11:25:47 -0500

    tablas en admin django

--- src/commons/admin.py
+++ src/commons/admin.py
@@ -4,10 +4,28 @@ from __future__ import absolute_import, unicode_literals
 from django import forms
 from django.contrib import admin
 
-from src.commons.models import Category, Need
+from src.commons.models import Category, Need, Shedule, StudentRequest
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
 
+admin.site.register(Category, CategoryAdmin)
+
+class NeedAdmin(admin.ModelAdmin):
+    list_display = ('name','category')
+
+admin.site.register(Need, NeedAdmin)
+
+class SheduleAdmin(admin.ModelAdmin):
+    list_display = ('user','date','time',)
+
+admin.site.register(Shedule, SheduleAdmin)
+
+class StudentRequestAdmin(admin.ModelAdmin):
+    list_display = ('student','need','shedule','status',    )
+
+admin.site.register(StudentRequest, StudentRequestAdmin)
 
 
 
