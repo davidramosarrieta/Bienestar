4cc90c56f18d7afa7782d4f19380caf7f60383fa
David Ramos <dramos@gravitalsoftware.com>
2017-08-15 12:11:12 -0500

    Auto stash before merge of "develop" and "origin/develop"

--- src/commons/schedules/views.py
+++ src/commons/schedules/views.py
@@ -10,5 +10,5 @@ def schedules_officers(request, template_name='dashboard/officers/schedules.html
         'yellow_trips': [],
         'red_trips': []
     }
-    a=1
+
     return render(request, template_name, data)
