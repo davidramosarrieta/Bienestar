1415e782a6195ba021b5964c259709aeae154858
David Ramos <dramos@gravitalsoftware.com>
2017-08-15 14:20:58 -0500

    Cambios en tablas

--- src/commons/schedules/views.py
+++ src/commons/schedules/views.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+
+from datetime import datetime
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
+from src.commons.models import Schedule
+
+
 @login_required
 def schedules_officers(request, template_name='dashboard/officers/schedules.html'):
-    data = {
-        'green_trips': [],
-        'yellow_trips': [],
-        'red_trips': []
-    }
+    data = {}
+    data['time'] = datetime.now()
+    data['schedules']= Schedule.objects.filter(user=request.user)
 
     return render(request, template_name, data)
